398048f70b8573b4f3c79fad0273bf5b497c351b
taehee28 <dev.thk28@gmail.com>
2022-07-10 22:23:01 +0900

    UI와 코드 연결하기

--- MusicPlayer/MusicPlayer/ViewController.swift
+++ MusicPlayer/MusicPlayer/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // 스토리보드의 UI 요소들과 연결되는 프로퍼티들
+    @IBOutlet var playPauseButton: UIButton!
+    @IBOutlet var timeLable: UILabel!
+    @IBOutlet var progressSlider: UISlider!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func touchUpPlayPauseButton(_ sender: UIButton) {
+        print("button tapped")
+    }
+    
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        print("slider value changed")
+    }
 }
 
