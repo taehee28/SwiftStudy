f79a86bd64eb7e0d8eb1cc6e1c8b9855b9b4cdca
taehee28 <dev.thk28@gmail.com>
2022-07-11 21:47:26 +0900

    구현코드 작성하기 1
    * 플레이어 재생 및 화면 업데이트에 필요한 함수 작성
    * Delegate 구현

--- MusicPlayer/MusicPlayer/ViewController.swift
+++ MusicPlayer/MusicPlayer/ViewController.swift
@@ -6,8 +6,12 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioPlayerDelegate {
+    
+    var player: AVAudioPlayer!
+    var timer: Timer!
     
     // 스토리보드의 UI 요소들과 연결되는 프로퍼티들
     @IBOutlet var playPauseButton: UIButton!
@@ -17,14 +21,104 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.initializePlayer()
+    }
+    
+    // MARK: 오디오 플레이어 초기화
+    func initializePlayer() {
+        guard let soundAsset = NSDataAsset(name: "sound") else {
+            print("음원 파일 에셋을 가져올 수 없습니다.")
+            return
+        }
+        
+        do {
+            try self.player = AVAudioPlayer(data: soundAsset.data)
+            self.player.delegate = self
+        } catch let error as NSError {
+            print("플레이어 초기화 실패: \(error.localizedDescription)")
+        }
+        
+        self.progressSlider.maximumValue = Float(self.player.duration)
+        self.progressSlider.minimumValue = 0
+        self.progressSlider.value = Float(self.player.currentTime)
+    }
+    
+    // MARK: 시간 라벨 텍스트 업데이트
+    func updateTimeLabelText(time: TimeInterval) {
+        let minute = Int(time / 60)
+        let second = Int(time.truncatingRemainder(dividingBy: 60))
+        let milisecond = Int(time.truncatingRemainder(dividingBy: 1) * 100)
+        
+        let timeText = String(format: "%02ld:%02ld:%02ld", minute, second, milisecond)
+        
+        self.timeLable.text = timeText
+    }
+    
+    // MARK: 타이머 생성 및 실행 함수
+    func makeAndFireTimer() {
+        self.timer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true, block: { [unowned self] (timer: Timer) in
+            if self.progressSlider.isTracking { return }
+            
+            self.updateTimeLabelText(time: self.player.currentTime)
+            self.progressSlider.value = Float(self.player.currentTime)
+        })
+        
+        self.timer.fire()
+    }
+    
+    // MARK: 타이머 초기화
+    func invalidateTimer() {
+        self.timer.invalidate()
+        self.timer = nil
+    }
+    
+    // MARK: AVAudioPlayerDelegate
+    func audioPlayerDecodeErrorDidOccur(_ player: AVAudioPlayer, error: Error?) {
+        guard let error = error else {
+            print("오디오 플레이어 디코드 오류 발생")
+            return
+        }
+        
+        let message = "오디오 플레이어 오류 발생 \(error.localizedDescription)"
+        
+        let alert = UIAlertController(title: "알림", message: message, preferredStyle: UIAlertController.Style.alert)
+        let okAction = UIAlertAction(title: "확인", style: UIAlertAction.Style.default) { (action: UIAlertAction) -> Void in
+            self.dismiss(animated: true, completion: nil)
+        }
+        
+        alert.addAction(okAction)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    // MARK: AVAudioPlayerDelegate
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        self.playPauseButton.isSelected = false
+        self.progressSlider.value = 0
+        self.updateTimeLabelText(time: 0)
+        self.invalidateTimer()
     }
 
     @IBAction func touchUpPlayPauseButton(_ sender: UIButton) {
         print("button tapped")
+        
+        sender.isSelected = !sender.isSelected
+        
+        if sender.isSelected {
+            self.player?.play()
+            self.makeAndFireTimer()
+        } else {
+            self.player?.pause()
+            self.invalidateTimer()
+        }
     }
     
     @IBAction func sliderValueChanged(_ sender: UISlider) {
-        print("slider value changed")
+        self.updateTimeLabelText(time: TimeInterval(sender.value))
+        
+        if sender.isTracking { return }
+        
+        self.player.currentTime = TimeInterval(sender.value)
     }
 }
 
